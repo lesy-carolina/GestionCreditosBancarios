eb86f486bce31e7e0b6832611244b613927b51e6
Frank <fcarpio113@gmail.com>
2025-09-11 18:11:59 -0500

    first commit

---
+++ ServicioPrestamos/src/main/java/org/nttdata/com/servicioprestamos/ServicioPrestamosApplication.java
@@ -0,0 +1,13 @@
+package org.nttdata.com.servicioprestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioPrestamosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioPrestamosApplication.class, args);
+    }
+
+}
---
+++ ServicioPrestamos/src/test/java/org/nttdata/com/servicioprestamos/ServicioPrestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nttdata.com.servicioprestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioPrestamosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
